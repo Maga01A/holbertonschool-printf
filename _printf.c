84db1b3829a8fd08616f76bd067f076e99574e4f
Maga01A <alnatluxanov@gmail.com>
2025-09-01 10:30:40 +0000

    fixed

--- _printf.c
+++ _printf.c
@@ -1,95 +1,56 @@
 #include "main.h"/**
- * print_number - prints an integer
- * @n: integer to print
+ * _printf - custom printf function
+ * @format: format string
  *
  * Return: number of characters printed
  */
-int print_number(int n)
-{
-	char buffer[12];
-	int i = 0, j, count = 0;
-	unsigned int num;	if (n == 0)
-	{
-		write(1, "0", 1);
-		return (1);
-	}	if (n < 0)
-	{
-		write(1, "-", 1);
-		count++;
-		num = -n;
-	}
-	else
-		num = n;	while (num > 0)
-	{
-		buffer[i++] = (num % 10) + '0';
-		num /= 10;
-	}	for (j = i - 1; j >= 0; j--)
-	{
-		write(1, &buffer[j], 1);
-		count++;
-	}	return (count);
-}/**
- * _printf - produces output according to a format
- * @format: format string containing directives
- *
- * Return: number of characters printed (excluding null byte),
- *         or -1 if format is NULL
- */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i = 0, count = 0;
-	char c, *str;	if (format == NULL)
-		return (-1);	va_start(args, format);	while (format[i] != '\0')
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == 'c')
-			{
-				c = (char)va_arg(args, int);
-				write(1, &c, 1);
-				count++;
-			}
-			else if (format[i] == 's')
-			{
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(null)";
-				while (*str)
-				{
-					write(1, str, 1);
-					str++;
-					count++;
-				}
-			}
-			else if (format[i] == 'd' || format[i] == 'i')
-			{
-				count += print_number(va_arg(args, int));
-			}
-			else if (format[i] == '%')
-			{
-				write(1, "%", 1);
-				count++;
-			}
-			else if (format[i] == '\0')
-			{
-				va_end(args);
-				return (-1);
-			}
-			else
-			{
-				write(1, "%", 1);
-				write(1, &format[i], 1);
-				count += 2;
-			}
-		}
-		else
-		{
-			write(1, &format[i], 1);
-			count++;
-		}
-		i++;
-	}	va_end(args);
-	return (count);
+    int count = 0;
+    va_list args;    if (!format)
+        return (-1);    va_start(args, format);    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;            switch (*format)
+            {
+            case 'c':
+                count += _putchar(va_arg(args, int));
+                break;
+            case 's':
+                count += print_string(va_arg(args, char *));
+                break;
+            case '%':
+                count += _putchar('%');
+                break;
+            case 'd':
+            case 'i':
+                count += print_number(va_arg(args, int));
+                break;
+            case 'u':
+                count += print_unsigned(va_arg(args, unsigned int));
+                break;
+            case 'o':
+                count += print_octal(va_arg(args, unsigned int));
+                break;
+            case 'x':
+                count += print_hex(va_arg(args, unsigned int), 0);
+                break;
+            case 'X':
+                count += print_hex(va_arg(args, unsigned int), 1);
+                break;
+            case 'p':
+                count += print_pointer(va_arg(args, void *));
+                break;
+            default:
+                count += _putchar('%');
+                count += _putchar(*format);
+                break;
+            }
+        }
+        else
+        {
+            count += _putchar(*format);
+        }        format++;
+    }    va_end(args);    return (count);
 }
