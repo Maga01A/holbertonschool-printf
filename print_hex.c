fdd57bc5c94fd1cc426c78e21ceba21bfbd48816
Maga01A <alnatluxanov@gmail.com>
2025-09-01 00:28:24 +0000

    compline code

---
+++ print_hex.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_hex - prints an unsigned int in hexadecimal
+ * @n: number to print
+ * @uppercase: 1 for uppercase letters, 0 for lowercase
+ *
+ * Return: number of characters printed
+ */
+int print_hex(unsigned int n, int uppercase)
+{
+    int count = 0;
+    char *hex = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+
+    if (n / 16)
+        count += print_hex(n / 16, uppercase);
+
+    count += _putchar(hex[n % 16]);
+
+    return (count);
+}
+
