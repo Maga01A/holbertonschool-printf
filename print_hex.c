fac70564171a3475e414841f90661cbe36dd69fd
Maga01A <alnatluxanov@gmail.com>
2025-09-03 15:15:05 +0400

    fixed

--- print_hex.c
+++ print_hex.c
@@ -9,14 +9,13 @@
  */
 int print_hex(unsigned int n, int uppercase)
 {
-    int count = 0;
-    char *hex = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+	int count = 0;
+	char *hex = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+	
+	if (n / 16)
+		count += print_hex(n / 16, uppercase);
 
-    if (n / 16)
-        count += print_hex(n / 16, uppercase);
+	count += _putchar(hex[n % 16]);
 
-    count += _putchar(hex[n % 16]);
-
-    return (count);
+	return (count);
 }
-
--- print_octal.c
+++ print_octal.c
@@ -8,13 +8,12 @@
  */
 int print_octal(unsigned int n)
 {
-    int count = 0;
+	int count = 0;
 
-    if (n / 8)
-        count += print_octal(n / 8);
+	if (n / 8)
+		count += print_octal(n / 8);
 
-    count += _putchar((n % 8) + '0');
+	count += _putchar((n % 8) + '0');
 
-    return (count);
+	return (count);
 }
-
