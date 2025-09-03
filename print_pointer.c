cea0fdf9114362d6d2c968d6049fd62a3f3211c9
Maga01A <alnatluxanov@gmail.com>
2025-09-03 15:18:03 +0400

    fixed

--- print_pointer.c
+++ print_pointer.c
@@ -8,13 +8,12 @@
  */
 int print_pointer(void *p)
 {
-    unsigned long addr = (unsigned long)p;
-    int count = 0;
+	unsigned long addr = (unsigned long)p;
+	int count = 0;
 
-    count += _putchar('0');
-    count += _putchar('x');
-    count += print_hex(addr, 0);
+	count += _putchar('0');
+	count += _putchar('x');
+	count += print_hex(addr, 0);
 
-    return (count);
+	return (count);
 }
-
