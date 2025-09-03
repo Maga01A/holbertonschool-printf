7f3d2726d36ddbdf92801b192bf12350b9897ed9
Maga01A <alnatluxanov@gmail.com>
2025-09-03 15:12:05 +0400

    fixed

--- _printf.c
+++ _printf.c
@@ -8,73 +8,73 @@
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
+	int count = 0;
+	va_list args;
 
-    if (!format)
-        return (-1);
+	if (!format)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
 
-            /* Eğer %'den sonra hiçbir şey yoksa hata (-1) dön */
-            if (!*format)
-            {
-                va_end(args);
-                return (-1);
-            }
+			/* Eğer %'den sonra karakter yoksa hata döndür */
+			if (!*format)
+			{
+				va_end(args);
+				return (-1);
+			}
 
-            switch (*format)
-            {
-            case 'c':
-                count += _putchar(va_arg(args, int));
-                break;
-            case 's':
-                count += print_string(va_arg(args, char *));
-                break;
-            case '%':
-                count += _putchar('%');
-                break;
-            case 'd':
-            case 'i':
-                count += print_number(va_arg(args, int));
-                break;
-            case 'u':
-                count += print_unsigned(va_arg(args, unsigned int));
-                break;
-            case 'o':
-                count += print_octal(va_arg(args, unsigned int));
-                break;
-            case 'x':
-                count += print_hex(va_arg(args, unsigned int), 0);
-                break;
-            case 'X':
-                count += print_hex(va_arg(args, unsigned int), 1);
-                break;
-            case 'p':
-                count += print_pointer(va_arg(args, void *));
-                break;
-            default:
-                count += _putchar('%');
-                count += _putchar(*format);
-                break;
-            }
-        }
-        else
-        {
-            count += _putchar(*format);
-        }
+			switch (*format)
+			{
+			case 'c':
+				count += _putchar(va_arg(args, int));
+				break;
+			case 's':
+				count += print_string(va_arg(args, char *));
+				break;
+			case '%':
+				count += _putchar('%');
+				break;
+			case 'd':
+			case 'i':
+				count += print_number(va_arg(args, int));
+				break;
+			case 'u':
+				count += print_unsigned(va_arg(args, unsigned int));
+				break;
+			case 'o':
+				count += print_octal(va_arg(args, unsigned int));
+				break;
+			case 'x':
+				count += print_hex(va_arg(args, unsigned int), 0);
+				break;
+			case 'X':
+				count += print_hex(va_arg(args, unsigned int), 1);
+				break;
+			case 'p':
+				count += print_pointer(va_arg(args, void *));
+				break;
+			default:
+				count += _putchar('%');
+				count += _putchar(*format);
+				break;
+			}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
 
-        format++;
-    }
+		format++;
+	}
 
-    va_end(args);
+	va_end(args);
 
-    return (count);
+	return (count);
 }
 
