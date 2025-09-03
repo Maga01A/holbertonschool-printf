83b9bb3fe4021a32dcb7a76918d6ea8f0c12b598
Maga01A <alnatluxanov@gmail.com>
2025-09-03 15:25:08 +0400

    fixed

--- _printf.c
+++ _printf.c
@@ -27,7 +27,7 @@ int handle_format(const char format, va_list args)
 		count += print_number(va_arg(args, int));
 		break;
 	case 'u':
-		count += print_unsigned(va_arg(args, unsigned int num));
+		count += print_unsigned(va_arg(args, unsigned int));
 		break;
 	case 'o':
 		count += print_octal(va_arg(args, unsigned int));
